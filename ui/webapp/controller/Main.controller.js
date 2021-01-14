16e737a8e6615cfee86663f244771fe9e5a345b0
Kevin Roth <kevin.roth@emmi.com>
2021-01-14 13:34:46 +0000

    added form from studentquiz

--- ui/webapp/controller/Main.controller.js
+++ ui/webapp/controller/Main.controller.js
@@ -3,5 +3,12 @@ sap.ui.define([
 ], function(Controller) {
   "use strict";
 
-  return Controller.extend("com.emmi.letsmove.controller.Main", {});
+  return Controller.extend("com.emmi.letsmove.controller.Main", {
+    onInit : function () {
+			// Get Random number between 1 and 11
+			var rand = Math.ceil(Math.random() * Math.floor(11));
+			//Set Random background image
+			this.byId("idAppControl").setBackgroundImage("resources/img/img_" + rand + ".jpg");
+		}
+  });
 });
