2c88276cdfdc7c1984e964ff2137258c4f043e92
Ben Krencker <benjamin.krencker@emmi.ch>
2021-01-15 15:00:32 +0000

    added Splash screen to ui5 app

--- ui/webapp/controller/Main.controller.js
+++ ui/webapp/controller/Main.controller.js
@@ -5,7 +5,9 @@ sap.ui.define([
 
     return Controller.extend("com.emmi.letsmove.controller.Main", {
         onInit: function () {
-            // Get Random number between 1 and 11
+          $("#splashScreen").remove();
+
+          // Get Random number between 1 and 11
             var rand = Math.ceil(Math.random() * Math.floor(11));
             //Set Random background image
             this.byId("idAppControl").setBackgroundImage("resources/img/img_" + rand + ".jpg");
