7684d90aa0bb3c57348edc4d58ae1cdc810b9f9c
Kevin Roth <kevin.roth@emmi.com>
2021-01-18 16:11:00 +0000

    added basic validation + smart bullet values

--- ui/webapp/controller/Main.controller.js
+++ ui/webapp/controller/Main.controller.js
@@ -1,69 +1,115 @@
 sap.ui.define([
-    "com/emmi/letsmove/controller/BaseController"
+  "com/emmi/letsmove/controller/BaseController"
 ], function (Controller) {
-    "use strict";
+  "use strict";
 
-    return Controller.extend("com.emmi.letsmove.controller.Main", {
-        onInit: function () {
-          $("#splashScreen").remove();
+  return Controller.extend("com.emmi.letsmove.controller.Main", {
+    onInit: function () {
+      $("#splashScreen").remove();
 
-          // Get Random number between 1 and 11
-            var rand = Math.ceil(Math.random() * Math.floor(11));
-            //Set Random background image
-            this.byId("idAppControl").setBackgroundImage("resources/img/img_" + rand + ".jpg");
+      // Get Random number between 1 and 11
+      var rand = Math.ceil(Math.random() * Math.floor(11));
+      //Set Random background image
+      this.byId("idAppControl").setBackgroundImage("resources/img/img_" + rand + ".jpg");
 
-            var oModel = new sap.ui.model.json.JSONModel();
+      var oModel = new sap.ui.model.json.JSONModel();
 
-            this.getView().setModel(oModel, "validation");
-            this.getModel("validation").setData({
-              type_code: null,
-              distance: null,
-              uom_code: null,
-              nickname: null,
-              company_code: null
-            });
+      this.getView().setModel(oModel, "validation");
+      this.getModel("validation").setData({
+        type_code: null,
+        distance: null,
+        uom_code: null,
+        nickname: null,
+        company_code: null
+      });
+      //Activate form validation
+      sap.ui.getCore().getMessageManager().registerObject(this.getView(), true);
+      this.loadProgressBar();
+    },
 
-        },
+    onAfterRendering: function () {
 
-        send: function () {
-          var formData = this.getModel("validation").getData();
-          this.createActivity(formData).then(function () {
-            this.getModel("validation").setData({});
-          }.bind(this));
-        },
+    },
 
-        createActivity: function (formData) {
-          var oListBinding = this.getModel().bindList("/Activities");
-          var oContext = oListBinding.create(formData, true);
-          var promise = new Promise(function (resolved) {
-            oContext.created().then(function () {
-              // Activity successfully created, delete form Data
-              resolved(oContext.getObject());
-            }.bind(this).bind(oContext));
-          }.bind(this));
+    send: function () {
+      var formData = this.getModel("validation").getData();
+      //Convert Float to string
+      formData.distance = formData.distance.toString();
 
-          return promise;
-        },
+      this.createActivity(formData).then(function () {
+        this.getModel("validation").setData({});
+      }.bind(this));
+    },
 
-        setNewIcon: function (oEvent) {
-            var currentSelectedButtonId = oEvent.getSource().oButton.getId();
+    createActivity: function (formData) {
+      var oListBinding = this.getModel().bindList("/Activities");
+      var oContext = oListBinding.create(formData, true);
+      var promise = new Promise(function (resolved) {
+        oContext.created().then(function () {
+          // Activity successfully created, delete form Data
+          resolved(oContext.getObject());
+        }.bind(this).bind(oContext));
+      }.bind(this));
 
-            if (this._lastActiveButton !== currentSelectedButtonId) {  //Prevent icon change when the same button has been pressed
-                this._lastActiveButton = currentSelectedButtonId;
-                var buttons = oEvent.getSource().getParent().getButtons();
+      return promise;
+    },
 
-                //Set new icon
-                for (var i in buttons) {
-                    var icon = buttons[i].getIcon();
-                    if (buttons[i].getId().toString() === currentSelectedButtonId.toString()) {
-                        icon = icon.replace(".png", "_white.png");
-                        buttons[i].setIcon(icon);
-                    } else {
-                        icon = icon.replace("_white.png", ".png");
-                        buttons[i].setIcon(icon);
-                    }
-                }
-            }
+    setNewIcon: function (oEvent) {
+      var currentSelectedButtonId = oEvent.getSource().oButton.getId();
+
+      if (this._lastActiveButton !== currentSelectedButtonId) {  //Prevent icon change when the same button has been pressed
+        this._lastActiveButton = currentSelectedButtonId;
+        var buttons = oEvent.getSource().getParent().getButtons();
+
+        //Set new icon
+        for (var i in buttons) {
+          var icon = buttons[i].getIcon();
+          if (buttons[i].getId().toString() === currentSelectedButtonId.toString()) {
+            icon = icon.replace(".png", "_white.png");
+            buttons[i].setIcon(icon);
+          } else {
+            icon = icon.replace("_white.png", ".png");
+            buttons[i].setIcon(icon);
+          }
         }
-    });
+      }
+    },
+
+    loadProgressBar: function () {
+      //Load Totals
+      var oListTotalActivity = this.getView().getModel().bindList("/TotalActivity");
+      new Promise(function (resolved, rejected) {
+        oListTotalActivity.requestContexts().then(function (aContexts) {
+          aContexts.forEach(function (oContext) {
+            var total = 0;
+            var unit = null;
+            if (oContext.getObject().totalKm <= 0) {
+              rejected();
+            } else {
+              //Use Miles when browser has one of the specified Locales
+              for (var i in navigator.languages) {
+                var loc = navigator.languages[i];
+
+                if (loc.includes("-US") || loc.includes("-GB") || loc.includes("-MM") || loc.includes("-LR")) {
+                  total = Math.round(oContext.getObject().totalMi);
+                  unit = 'mi';
+                  break;
+                } else {
+                  total = Math.round(oContext.getObject().totalKm);
+                  unit = 'km';
+                  break;
+                }
+              }
+              //Set SmartBullet Values
+              this.getView().byId("microBulletChart").setTargetValue(30000);
+              this.getView().byId("microBulletChart").setTargetValueLabel("30000 " + unit);
+              this.getView().byId("microBulletChartActualData").setValue(total);
+              this.getView().byId("microBulletChart").setForecastValue(40000);
+              resolved(true);
+            }
+          }.bind(this));
+        }.bind(this));
+      }.bind(this));
+    }
+  });
 });
