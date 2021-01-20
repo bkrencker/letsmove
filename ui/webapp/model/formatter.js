be4079d270299d7d10c42e28826c97c256e576e5
Kevin Roth <kevin.roth@emmi.com>
2021-01-20 08:21:13 +0000

    replaced icon replacement in controller with a formatter in the view

--- ui/webapp/model/formatter.js
+++ ui/webapp/model/formatter.js
@@ -1,4 +1,15 @@
 sap.ui.define([], function () {
-	"use strict";
-	return {};
+  "use strict";
+  return {
+    /*
+    * Show white icon when segment has been selected
+    */
+    setIcon: function (typeCode, code) {
+      if (typeCode === code) {
+        return `resources/img/icons/${code}_white.png`;
+      } else {
+        return `resources/img/icons/${code}.png`;
+      }
+    }
+  };
 });
