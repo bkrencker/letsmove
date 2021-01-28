8db7b04d4693b9ad70c3ebacc2d93dd40bbed97e
Ben Krencker <benjamin.krencker@emmi.ch>
2021-01-28 18:22:03 +0000

    imageUrl needed for Admin-Area!!

--- srv/admin-service.js
+++ srv/admin-service.js
@@ -7,8 +7,15 @@ const {
 
 module.exports = srv => {
 
-  srv.after('INSERT', 'Activities', (req) => {
+  srv.after('READ', 'Activities', (each) => {
+    if (each.type && each.type.code) {
+      each.imageUrl = 'activities/webapp/icons/' + each.type.code + '.png';
+    } else if (each.type_code) {
+      each.imageUrl = 'activities/webapp/icons/' + each.type_code + '.png';
+    }
+  });
 
+  srv.after('INSERT', 'Activities', (req) => {
     wss.clients.forEach(function each(client) {
       if (client.readyState === WebSocket.OPEN) {
         client.send("refresh");
