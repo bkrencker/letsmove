401edbdaf14cccd9c5dd76a25c27055bf64b2bff
Kevin Roth <kevin.roth@emmi.com>
2021-01-29 09:27:54 +0000

    added new background image

--- ui/webapp/controller/Main.controller.js
+++ ui/webapp/controller/Main.controller.js
@@ -12,7 +12,7 @@ sap.ui.define([
       // Get Random number between 1 and 11
       var rand = Math.ceil(Math.random() * Math.floor(11));
       //Set Random background image
-      this.byId("idAppControl").setBackgroundImage("resources/img/img_" + rand + ".jpg");
+      this.byId("idAppControl").setBackgroundImage("resources/img/background.png");
 
       var oModel = new sap.ui.model.json.JSONModel();
       var oViewModel = new sap.ui.model.json.JSONModel();
